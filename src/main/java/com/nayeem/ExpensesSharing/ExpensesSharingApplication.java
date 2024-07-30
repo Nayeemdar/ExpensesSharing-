53584655fad9dea7530221e9e2be2051885d4e84
Nayeem Ahmad Dar <Nayeemdar3@gmail.com>
2024-07-30 20:02:47 +0530

    Initial commit

---
+++ src/main/java/com/nayeem/ExpensesSharing/ExpensesSharingApplication.java
@@ -0,0 +1,13 @@
+package com.nayeem.ExpensesSharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nayeem/ExpensesSharing/ExpensesSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayeem.ExpensesSharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
